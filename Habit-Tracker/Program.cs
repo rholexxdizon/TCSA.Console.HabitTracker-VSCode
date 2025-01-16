8b67a6100c48eb7738bd560165c7ddf4c337d83d
rholexxdizon <dizonrholex02@gmail.com>
2025-01-16 22:31:52 +0800

    Add project files.

---
+++ Habit-Tracker/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+ namespace Habit_Tracker
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=habit-tracker.db";
+
+            using (var connection = new SqliteConnection(connectionString)){
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                
+                tableCmd.CommandText = 
+                @"CREATE TABLE IF NOT EXISTS drinking_water (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TEXT,
+                    Quanitity INTEGER
+                    )";
+
+                tableCmd.ExecuteNonQuery();
+                
+                connection.Close();
+            }
+        }
+
+        static void GetUserInput()
+        {
+            Console.Clear();
+            bool closeApp = false;
+            while (closeApp == false)
+            {
+                Console.WriteLine("\n\nMAIN MENU");
+                Console.WriteLine("\n\nWhat would you like to do?");
+                Console.WriteLine("\n\nType 0 to Close Application");
+                Console.WriteLine("\n\nType 1 to View All Records.");
+                Console.WriteLine("\n\nType 2 to Insert Record.");
+                Console.WriteLine("\n\nType 3 to Delete Record.");
+                Console.WriteLine("\n\nType 4 to Update Record.");
+                Console.WriteLine("-------------------------------------------------------------\n");
+            }
+        }
+    }
+
+ }
